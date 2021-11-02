85b6fa1627331afb0583aec2fcd179797e66d983
raccchel <66702203+raccchel@users.noreply.github.com>
2021-11-02 13:11:21 -0400

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,27 @@
 class PostsController < SessionsController
 
   def index
+    @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_info)
+    
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def post_info
+    info.require(:post).permit(:title, :content)
   end
 
 end
