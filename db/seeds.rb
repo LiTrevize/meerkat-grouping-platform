0a64b4b0d8011498dcf2bffec4d5a6934af8958a
Jingyu Li <jingyu.li@columbia.edu>
2021-11-16 18:20:05 -0500

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,15 +39,19 @@ Nickname.create([
 ])
 profile = Profile.create!(user_id: user.id, school: 'SEAS', degree: 'MS', major: 'CS')
 # post
-post = Post.create!(user_id: user.id, title: '4995 SAAS Project Team', content: "I would like to find teammates for Project Meerkat.", next_nickname_id: 2, low_number: 3, high_number: 4, tag1: 'coursework')
+post = Post.create!(user_id: user.id, title: '4995 SAAS Project Team', content: "I would like to find teammates for Project Meerkat.", next_nickname_id: 3, low_number: 3, high_number: 4, tag1: 'coursework')
 group = Group.create(post_id: post.id)
 GroupUser.create(group_id: group.id, user_id: user1.id, status: :accepted)
 GroupUser.create(group_id: group.id, user_id: user2.id, status: :accepted)
 # comments
 Comment.create!(post_id: post.id, content: "What tech stack will you use?", is_public: true, from_user_id: user1.id)
-Comment.create!(post_id: post.id, content: "Ruby on Rails", is_public: false, from_user_id: user.id)
+Comment.create!(post_id: post.id, content: "Ruby on Rails", is_public: true, from_user_id: user.id)
+Comment.create!(post_id: post.id, content: "How's your background?", is_public: true, from_user_id: user2.id)
+Comment.create!(post_id: post.id, content: "I have one year experience of full stack development", is_public: true, from_user_id: user.id)
+
 PostUserNickname.create([
-    {post_id: post.id, user_id: user1.id, nickname_id: 1}
+    {post_id: post.id, user_id: user1.id, nickname_id: 1},
+    {post_id: post.id, user_id: user2.id, nickname_id: 2}
 ])
 
 # test_approved_wq=GroupUser.create!(group_id: group.id, user_id: 1, status: :approved)
