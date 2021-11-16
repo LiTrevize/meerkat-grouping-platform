64973b12624aa3979cbb11ee98947045197936d8
Frank Zhang <frankzhang@dyn-160-39-210-138.dyn.columbia.edu>
2021-11-16 16:29:01 -0500

    tag cucumber

--- spec/controllers/profile_spec.rb
+++ spec/controllers/profile_spec.rb
@@ -1,57 +1,15 @@
 require 'rails_helper'
 
-# describe User do
-#     user_id = -1
-#     it "should put the user info" do
-#       user = User.create(:name => 't1', :email => 't1@columbia.edu' )
-#       user_id = user.id
-#       #   puts user.id
-#       expect(user.name).to eq('t1')
-#       expect(user.email).to eq('t1@columbia.edu')
-#     end
-    
-    
-
-#     describe "profile test" do
-#         # puts "hi!!!!~!"
-#         hi = Profile.create(:school => 'seas', :degree => 'bs', :major => 'cs' )
-#         it "create new profile" do
-#           expect(hi.school).to eq('seas')
-#           expect(hi.degree).to eq('bs')
-#           expect(hi.major).to eq('cs')
-#         end
-#         # it "update the infor" do 
-          
-        
-#     end
-#   end
-# it 'selects the Search Results template for rendering'
-# it 'makes the TMDb search results available to that 
-# template'
 
 describe ProfilesController do
   render_views
 
   before :each do
-    # User.where(id: 1).first_or_initialize do |user|
-    #   user.name = 't1'
-    #   user.email = 't1@columbia.edu'
-    # end
     @user = User.create(:id => 1, :name => 't1', :email => 't1@columbia.edu' )
-    # @tmp_profile = Profile.create(:user_id => @user.id, :school => 'seas', :degree => 'bs', :major => 'cs' )
 
   end
   
-  # describe "create profile" do
-  #   it 'calls the model method that create profile' do
-  #     url = "/profile/"
-  #     post :create, params: { profile: {degree: "ms", school: "seas"}}
 
-  #     new_profile = Profile.find_by_user_id(@user.id)
-  #     expect(new_profile.degree).to eq 'ms'
-  #     # profile.find_by_user_id
-  #   end
-  # end
   describe "test update profile" do
     it 'calls the model method that update profile' do
       @tmp_profile = Profile.create(:user_id => @user.id, :school => 'seas', :degree => 'bs', :major => 'cs' )
