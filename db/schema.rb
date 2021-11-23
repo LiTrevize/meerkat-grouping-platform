6e3f7c05e476b8f8c78cbd0039afe5c063746bea
Rachel Liu <raccchel@wanchendembp.myfiosgateway.com>
2021-11-23 03:43:55 -0500

    comment ui modified

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,9 @@ ActiveRecord::Schema.define(version: 2021_11_21_194236) do
     t.integer "next_nickname_id", default: 1
     t.integer "low_number"
     t.integer "high_number"
+    t.string "tag1"
+    t.string "tag2"
+    t.string "tag3"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
@@ -94,8 +97,9 @@ ActiveRecord::Schema.define(version: 2021_11_21_194236) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-  create_table "tags", primary_key: "name", id: :string, force: :cascade do |t|
-    t.integer "freq", default: 0
+  create_table "tags", id: false, force: :cascade do |t|
+    t.string "name"
+    t.integer "freq"
   end
 
   create_table "users", force: :cascade do |t|
