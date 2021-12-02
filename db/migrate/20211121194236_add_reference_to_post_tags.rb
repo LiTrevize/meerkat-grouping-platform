3ae5fa0683d5832d65b9d471b3940d7aade54682
Jingyu Li <jingyu.li@columbia.edu>
2021-12-02 16:43:42 -0500

    fix tag name fk migration

--- db/migrate/20211121194236_add_reference_to_post_tags.rb
+++ db/migrate/20211121194236_add_reference_to_post_tags.rb
@@ -1,7 +1,7 @@
 class AddReferenceToPostTags < ActiveRecord::Migration[5.2]
   def change
     add_column :post_tags, :tag_name, :string
-    add_foreign_key :post_tags, :tags, column: :tag_name
+    add_foreign_key :post_tags, :tags, column: :tag_name, primary_key: :name
     add_index :post_tags, :tag_name
   end
 end
