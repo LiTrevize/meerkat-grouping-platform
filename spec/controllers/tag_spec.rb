bc8909af9e33f0c1b2adeda40b09a4101dc93a36
Frank Zhang <frankzhang@dyn-160-39-211-200.dyn.columbia.edu>
2021-12-12 15:14:25 -0500

    tag check

--- spec/controllers/tag_spec.rb
+++ spec/controllers/tag_spec.rb
@@ -8,6 +8,11 @@ describe PostsController do
     @user = User.create!(:id => 1, :name => 't1', :email => 't1@columbia.edu' )
     @tag = Tag.create(name:"ab", freq: 0)
     @tag = Tag.create(name:"bc", freq: 0)
+
+    @tag1 = Tag.create!(name: "tag_a", freq: "10")
+    @tag2 = Tag.create!(name: "tag_b", freq: "1")
+    @tag3 = Tag.create!(name: "tag_c", freq: "12")
+
   end
  
   
@@ -21,6 +26,19 @@ describe PostsController do
 
     end
   end
+
+
+  describe "select tags" do
+    it 'hide tags' do
+      # post :approve, params: {id: test_approve_group2.id, user_id: test_applican1.id}
+      tmp_hash = { @tag1.name => 1, @tag2.name => 1}
+      get :index, params: {tagnames: tmp_hash}
+      puts(response.body)
+      expect(response.body).to include 'tag_a'
+      expect(response.body).to include 'tag_b'
+    end
+  end
+   
 
   # describe "test new posts with tags" do
   #   it 'calls the model method that direct to new profile' do
